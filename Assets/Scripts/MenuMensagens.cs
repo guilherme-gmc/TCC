7671af2490724c30a1e2a69091ca5a29e40c8e9c
Pedro Mendes Adorno <pedroadorno99@gmail.com>
2019-04-06 21:40:27 -0300

    Menu de mensagens implementado e com wrap-around

--- Assets/Scripts/MenuMensagens.cs
+++ Assets/Scripts/MenuMensagens.cs
@@ -1,19 +1,51 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuMensagens : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    DirectoryInfo msgFolder;
+    private int i;
+    private GameObject msg;
+    private Image msgImg;
+    private string msgsPath;
+    private Sprite[] msgSprites;
+
+    // Use this for initialization
+    void Start () {
+        msgsPath = "Mensagens/";
+        msg = GameObject.Find("Message");
+        msgImg = msg.GetComponent<Image>();
+        i = 0;
+
+        msgSprites = Resources.LoadAll<Sprite>(msgsPath);
+
+        SetSprite(i);
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	public void CarregarInicio(){
+	public void CarregarInicio() {
 		UnityEngine.SceneManagement.SceneManager.LoadScene ("MenuInicial");
 	}
+
+    public void NextMessage() {
+        SetSprite(i + 1);
+    }
+
+    public void PreviousMessage()
+    {
+        SetSprite(i - 1);
+    }
+
+    private void SetSprite(int index) {
+        if (index == msgSprites.Length)
+        {
+            index = 0;
+        } else if(index < 0)
+        {
+            index = msgSprites.Length - 1;
+        }
+        i = index;
+        msgImg.sprite = msgSprites[index];
+    }
 }
