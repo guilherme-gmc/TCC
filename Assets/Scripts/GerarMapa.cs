ee1fbc0d5d0c14fcc9778d7c5117b20d52f9e073
Guilherme Medeiros de Carvalho <guilhermemedeirosgmc@gmail.com>
2019-04-18 21:28:53 -0300

    Coloquei o consumivel

--- Assets/Scripts/GerarMapa.cs
+++ Assets/Scripts/GerarMapa.cs
@@ -14,6 +14,7 @@ public class GerarMapa : MonoBehaviour
 
     private float groundY;
     private GameObject enemy1;
+	private GameObject choco;
 
     //Enemy1 values
     private float enemy1_minY;
@@ -21,6 +22,12 @@ public class GerarMapa : MonoBehaviour
     private float enemy1_spawnPadding;
     private float enemy1_spawnDelay;
     private float enemy1_spawnRepeat;
+
+
+	//Valores consumivel
+
+	private float cons_spawnDelay;
+	private float cons_spawnRepeat;
     
     // Use this for initialization
     void Start()
@@ -29,7 +36,7 @@ public class GerarMapa : MonoBehaviour
         groundY = Chao.transform.position.y;
         Chao.transform.position = new Vector3(Chao.transform.position.x, groundY, Chao.transform.position.z);
         Chao2.transform.position = new Vector3(Chao2.transform.position.x, groundY, Chao2.transform.position.z);
-
+		//spawn do inimigo
         enemy1 = Resources.Load<GameObject>("Prefabs/enemy1");
         enemy1_spawnPadding = 0f;
         enemy1_minY = groundY + Chao.transform.localScale.y / 2 + enemy1.transform.localScale.y / 2 + enemy1_spawnPadding;
@@ -38,6 +45,15 @@ public class GerarMapa : MonoBehaviour
         enemy1_spawnRepeat = 1.5f;
 
         InvokeRepeating("SpawnEnemy1", enemy1_spawnDelay, enemy1_spawnRepeat);
+
+		//spawn consumivel
+		choco = Resources.Load<GameObject>("Prefabs/choco");
+
+		cons_spawnDelay = 3f;
+		cons_spawnRepeat = 10f;
+
+		InvokeRepeating("SpawnCons", cons_spawnDelay, cons_spawnRepeat);
+
     }
 
     // Update is called once per frame
@@ -60,4 +76,10 @@ public class GerarMapa : MonoBehaviour
     void SpawnEnemy1() {
         Instantiate(enemy1, new Vector3(Camera.main.gameObject.transform.position.x + Camera.main.orthographicSize * 2, Random.Range(enemy1_minY, enemy1_maxY), 0f), Quaternion.identity);
     }
+
+	void SpawnCons() {
+		Instantiate(choco, new Vector3(Camera.main.gameObject.transform.position.x + Camera.main.orthographicSize * 2, Random.Range(enemy1_minY, enemy1_maxY), 0f), Quaternion.identity);
+	}
 }
+
+
