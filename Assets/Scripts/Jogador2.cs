eebe508572c2d4da27c8c628635680f2da19c203
Guilherme Medeiros de Carvalho <guilhermemedeirosgmc@gmail.com>
2019-04-24 21:58:06 -0300

    Adicionei a tela do boss e mudei o movimento nela
    Nessa nova fase, a pessoa vai se movimentar pelo WASD ou pelas setas e atirar pelo botão esquero do mouse.
    Coloquei apenas o movimento até então

---
+++ Assets/Scripts/Jogador2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador2 : MonoBehaviour
+{
+	float vel = 5f;
+
+	 // Start is called before the first frame update
+    void Start()
+    {
+		
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		float vely = Input.GetAxis("Vertical") * vel  * Time.deltaTime;
+		float velx = Input.GetAxis("Horizontal") * vel * Time.deltaTime;
+		transform.Translate(velx,vely,0);
+    }
+}
