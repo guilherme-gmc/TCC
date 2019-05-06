e453030122863fc7967a9c27559d77f1621221d3
Guilherme Medeiros de Carvalho <guilhermemedeirosgmc@gmail.com>
2019-05-06 10:25:51 -0300

    Mano, n sei qq ta acontecendo. To em desespero

---
+++ Assets/Consumivel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumivel : MonoBehaviour
+{
+	private Rigidbody2D body;
+	private Vector2 hspd;
+	// Start is called before the first frame update
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+		hspd = new Vector2(-200f, 0f);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		body.velocity = hspd * Time.deltaTime;
+	}
+}
---
+++ Assets/IntroAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAnim : MonoBehaviour
+{
+    private static bool openingApp = true;
+    private GameObject introAnim;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        introAnim = transform.Find("IntroAnim").gameObject;
+        anim = introAnim.GetComponent<Animator>();
+        if(openingApp) {
+            anim.SetTrigger("introAnim");
+            openingApp = false;
+        } else {
+            anim.SetTrigger("introIdle");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
